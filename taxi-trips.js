dcec60313575dd4ee161e3cab478b98f8a49075f
v203 <vuyisamr203@gmail.com>
2021-12-15 13:56:41 +0200

    final update on taxi functions

--- taxi-trips.js
+++ taxi-trips.js
@@ -2,34 +2,32 @@ module.exports = function TaxiTrips(pool){
 
     let totalTripCount = async ()=>{
         let results = await pool.query(`select count(*) from trip`);
-        return Number(results.rows[0]['count'])
+        return Number(results.rows[0]['count']);
 
     }
 
     let findAllRegions = async ()=>{
- 
       let results  = await pool.query(`Select id from region`);
-        return results.rows
+        return results.rows;
     }
 
-    let findTaxisForRegion = async (taxis)=>{
-        results = await pool.query(`SELECT * FROM trip where region_id = `);
-        return results.rows
+    let findTaxisForRegion = async (region)=>{
+        results = await pool.query(`SELECT reg_number FROM taxi join region on region.id = taxi.taxi_region_id where region.name = '${region}'`);
+        return results.rows;
     }
 
-    let findTripsByRegNumber = async ()=>{
-        let results  = await pool.query(``);
-
+    let findTripsByRegNumber = async (reg_number)=>{
+        let results  = await pool.query(`select * from trip where trip.trip_reg_number = '${reg_number}'`);
+        return results.rows;
     }
     let findTripsByRegion = async (region)=>{
         let results = await pool.query(`select count(*) from trip join region on region.id = trip.trip_region where '${region}' = region.name`);
-        return Number(results.rows[0]['count'])
+        return Number(results.rows[0]['count']);
 
     }
     let findIncomeByRegNumber = async (par)=>{
         let results = await pool.query(`select trip_reg_number ,sum(fare_total) from trip where trip.trip_reg_number = '${par}' group by trip_reg_number`);
-        return results.rows['0']
-
+        return results.rows['0'];
     }
 
     let findTotalIncomePerTaxi = async ()=>{
@@ -38,11 +36,11 @@ module.exports = function TaxiTrips(pool){
 
     let findTotalIncome = async ()=>{
         let results = await pool.query(`select sum(fare_total) from trip`);
-        return results.rows[0]['sum']
-
+        return results.rows[0]['sum'];
     }
 
-    let findTotalIncomeByRegion = async ()=>{
+    let findTotalIncomeByRegion = async (region)=>{
+      return  (await pool.query(`select sum(fare_total) from trip join region on region.id = trip.trip_region where region.name = '${region}' group by region.name`)).rows[0]["sum"];
 
     }
 
@@ -58,7 +56,7 @@ return{
     findTotalIncome,
     findTotalIncomeByRegion
 
-}
+};
     
 	
-}
+};
--- test/taxi-trips.test.js
+++ test/taxi-trips.test.js
@@ -20,7 +20,7 @@ describe('Taxi Trips', function () {
 
 
 
-    it.skip('should find all the regions', async function () {
+    it('should find all the regions', async function () {
 
         const taxiTrips = TaxiTrips(pool);
         let expected = 3
@@ -29,25 +29,60 @@ describe('Taxi Trips', function () {
 
     });
 
-    // it('should find all the taxis for a region', async function () {
-    //     const taxiTrips = TaxiTrips(pool);
+    it('should find all the taxis for a region', async function () {
+        const taxiTrips = TaxiTrips(pool);
 
-    //     assert.deepStrictEqual([],await taxiTrips.findTaxisForRegion('Durban'));
-    //     assert.deepStrictEqual([],await taxiTrips.findTaxisForRegion('Cape Town'));
-    //     assert.deepStrictEqual([],await taxiTrips.findTaxisForRegion('Gauteng'));
+        let capeTownTaxis = [{ "reg_number": "CA1111" }, { "reg_number": "CY5555" }, { "reg_number": "CJ2222" }]
+        let durbanTaxis = [{ "reg_number": "ZN1111" }, { "reg_number": "ZN5555" }, { "reg_number": "ZN2222" }]
+        let kznTaxis = [{ "reg_number": "DZ1111" }, { "reg_number": "DD5555" }, { "reg_number": "DH2222" }];
 
-    // })
+        assert.deepStrictEqual(durbanTaxis, await taxiTrips.findTaxisForRegion('Durban'));
+        assert.deepStrictEqual(capeTownTaxis, await taxiTrips.findTaxisForRegion('Cape Town'));
+        assert.deepStrictEqual(kznTaxis, await taxiTrips.findTaxisForRegion('Gauteng'));
 
-    // it('should find all the trips for a reg number', async function () {
+    })
 
-    //     const taxiTrips = TaxiTrips(pool);
+    it('should find all the trips for a reg number', async function () {
 
-    //     assert.deepStrictEqual([],await taxiTrips.findTripsByRegNumber('...'));
-    //     assert.deepStrictEqual([],await taxiTrips.findTripsByRegNumber('***'));
+        const taxiTrips = TaxiTrips(pool);
 
-    // });
+        let DZ1111trips = [{ fare_total: '119.00', trip_reg_number: 'DZ1111', trip_region: 3, trip_route: 7 }, { fare_total: '119.00', trip_reg_number: 'DZ1111', trip_region: 3, trip_route: 7 },
+        { fare_total: '119.00', trip_reg_number: 'DZ1111', trip_region: 3, trip_route: 8 },
+        { fare_total: '119.00', trip_reg_number: 'DZ1111', trip_region: 3, trip_route: 9 }]
 
-    it.skip('should find the total number of trips by region', async function () {
+        let DD5555trips = [
+            {
+              trip_route: 7,
+              trip_reg_number: 'DD5555',
+              trip_region: 3,
+              fare_total: '119.00'
+            },
+            {
+              trip_route: 8,
+              trip_reg_number: 'DD5555',
+              trip_region: 3,
+              fare_total: '119.00'
+            },
+            {
+              trip_route: 9,
+              trip_reg_number: 'DD5555',
+              trip_region: 3,
+              fare_total: '119.00'
+            },
+            {
+              trip_route: 9,
+              trip_reg_number: 'DD5555',
+              trip_region: 3,
+              fare_total: '119.00'
+            }
+          ]
+          
+        assert.deepStrictEqual(DZ1111trips, await taxiTrips.findTripsByRegNumber('DZ1111'));
+        assert.deepStrictEqual(DD5555trips,await taxiTrips.findTripsByRegNumber('DD5555'));
+
+    });
+
+    it('should find the total number of trips by region', async function () {
 
         const taxiTrips = TaxiTrips(pool);
 
@@ -57,15 +92,15 @@ describe('Taxi Trips', function () {
 
     });
 
-    it.skip('find the total income for a given reg number', async function () {
+    it('find the total income for a given reg number', async function () {
 
         const taxiTrips = TaxiTrips(pool);
-        assert.deepStrictEqual({sum: '756.00',trip_reg_number: 'ZN1111'}, await taxiTrips.findIncomeByRegNumber('ZN1111'));
-        assert.deepStrictEqual({sum: '476.00',trip_reg_number: 'DZ1111'}, await taxiTrips.findIncomeByRegNumber('DZ1111'));
+        assert.deepStrictEqual({ sum: '756.00', trip_reg_number: 'ZN1111' }, await taxiTrips.findIncomeByRegNumber('ZN1111'));
+        assert.deepStrictEqual({ sum: '476.00', trip_reg_number: 'DZ1111' }, await taxiTrips.findIncomeByRegNumber('DZ1111'));
 
     });
 
-    it.skip('find the total income for each taxi', async function () {
+    it('find the total income for each taxi', async function () {
 
         const taxiTrips = TaxiTrips(pool);
 
@@ -84,15 +119,23 @@ describe('Taxi Trips', function () {
 
     });
 
-    it.skip('find the total income for all the taxis', async function () {
+    it('find the total income for all the taxis', async function () {
         const taxiTrips = TaxiTrips(pool);
         assert.deepStrictEqual(5943.00, parseFloat(await taxiTrips.findTotalIncome()));
     });
 
-    it.skip('find the total trip count for all the taxis', async function () {
+    it('find the total trip count for all the taxis', async function () {
         const taxiTrips = TaxiTrips(pool);
         assert.deepStrictEqual(38, await taxiTrips.totalTripCount());
     });
+
+    it("Find the income of  specific region",async ()=>{
+        const taxiTrips = TaxiTrips(pool);
+        let actual = await taxiTrips.findTotalIncomeByRegion("Durban");
+        let expected = '2296.00'
+        assert.deepStrictEqual(expected,actual);
+
+    })
 
 
     after(function () {
